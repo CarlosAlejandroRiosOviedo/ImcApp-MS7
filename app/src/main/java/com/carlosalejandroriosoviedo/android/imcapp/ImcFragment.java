bcecffddd2a58eb7b54ee595a325b93c135cad70
Carlos Alejandro Rios Oviedo <14470272@itcampeche.edu.mx>
2017-11-12 09:35:57 -0800

    Version inicial del proyecto ImcApp con fragmentos. No incluye Manejo de Eventos.

---
+++ app/src/main/java/com/carlosalejandroriosoviedo/android/imcapp/ImcFragment.java
@@ -0,0 +1,26 @@
+package com.carlosalejandroriosoviedo.android.imcapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ImcFragment extends Fragment{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v  = inflater.inflate(R.layout.fragmento_imc, container, false);
+
+        return v;
+    }
+}
